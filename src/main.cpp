9c789566e62a1028efd2eff114f67851171f3e8c
Lillie <75392499+iLillie@users.noreply.github.com>
2025-12-12 17:56:13 +0100

    update to unity 6 and remove autohooks

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,31 @@
 #include "main.hpp"
-
-#include "autohooks/shared/hooks.hpp"
 #include "beatsaber-hook/shared/utils/il2cpp-functions.hpp"
 #include "logger.hpp"
 #include "modInfo.hpp"
 #include "scotland2/shared/modloader.h"
+#include "GlobalNamespace/PlayerAgreements.hpp"
+#include "GlobalNamespace/PlayerData.hpp"
+#include "GlobalNamespace/PlayerDataFileModel.hpp"
+#include "GlobalNamespace/PlayerDataModel.hpp"
+#include "GlobalNamespace/PlayerSensitivityFlag.hpp"
+#include "OculusStudios/Platform/Core/UserAgeCategory.hpp"
+#include "beatsaber-hook/shared/utils/hooking.hpp"
+
+MAKE_HOOK_MATCH(
+    PlayerDataFileModel_Load,
+    &GlobalNamespace::PlayerDataFileModel::LoadOrCreateFromJsonString,
+    GlobalNamespace::PlayerData*,
+    GlobalNamespace::PlayerDataFileModel* self,
+    StringW jsonString
+) {
+    auto value = PlayerDataFileModel_Load(self, jsonString);
+    if (value->userAgeCategory != OculusStudios::Platform::Core::UserAgeCategory::Adult) {
+        value->userAgeCategory = OculusStudios::Platform::Core::UserAgeCategory::Adult;
+        value->desiredSensitivityFlag = GlobalNamespace::PlayerSensitivityFlag::Explicit;
+        value->playerAgreements->playerSensitivityFlagVersion = 3;
+    }
+    return value;
+}
 
 /// @brief Called at the early stages of game loading
 /// @param info The mod info.  Update this with your mod's info.
@@ -12,7 +33,6 @@
 MOD_EXPORT_FUNC void setup(CModInfo& info) {
     // Convert the mod info to a C struct and set that as the modloader info.
     info = modInfo.to_c();
-
     Logger.info("Completed setup!");
 }
 
@@ -21,28 +41,13 @@ MOD_EXPORT_FUNC void setup(CModInfo& info) {
 MOD_EXPORT_FUNC void load() {
     // Initialize il2cpp functions
     il2cpp_functions::Init();
-
-    // Get the number of early hooks that will be installed.
-    auto earlyHookCount = EARLY_HOOK_COUNT;
-
-    // install early hooks
-    if (earlyHookCount > 0) {
-        Logger.info("Installing {} early hook{}", earlyHookCount, earlyHookCount == 0 || earlyHookCount > 1 ? "s" : "");
-        INSTALL_EARLY_HOOKS();
-        Logger.info("Finished installing early hook{}", earlyHookCount == 0 || earlyHookCount > 1 ? "s" : "");
-    }
 }
 
 /// @brief Called later on in the game loading - a good time to install function hooks
 /// @return
 MOD_EXPORT_FUNC void late_load() {
     // Get the number of late hooks that will be installed.
-    auto lateHookCount = LATE_HOOK_COUNT;
-
-    // Install late hooks
-    if (lateHookCount > 0) {
-        Logger.info("Installing {} late hook{}", lateHookCount, lateHookCount > 1 ? "s" : "");
-        INSTALL_LATE_HOOKS();
-        Logger.info("Finished installing late hook{}", lateHookCount == 0 || lateHookCount > 1 ? "s" : "");
-    }
+    Logger.info("Installing 1 late hook");
+    INSTALL_HOOK(Logger, PlayerDataFileModel_Load);
+    Logger.info("Finished installing late hook");
 }
